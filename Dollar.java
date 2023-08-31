3b6b12ec3f82fd8e47c49429b195fc28280aba5f
Dnyanesh Auti <142774332+autidnyanesh@users.noreply.github.com>
2023-08-31 10:53:02 +0530

    Dollar.java

--- Dollar.java
+++ Dollar.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Dollar{
     public static void main(String[] args) {
-        float dollars;
+        double dollars;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter dollars:");
-        dollars = sc.nextLong();
-        float rupees = dollars * 82;
+        System.out.print("Enter dollars:");
+        dollars = sc.nextDouble();
+        double rupees = dollars * 82.65;
         System.out.println(rupees + " Rupees");
 
     }
