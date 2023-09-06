76f52769573e03c9e8651eba7e7cf33e921fa0af
Dnyanesh Auti <142774332+autidnyanesh@users.noreply.github.com>
2023-09-06 11:15:25 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,23 +1,36 @@
+
 import java.util.Scanner;
 
 public class LinearSearch {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter the size of the array: ");
-            int size = scanner.nextInt();
+        System.out.print("Enter the length of the array: ");
+        int length = scanner.nextInt();
 
-            int[] arr = new int[size];
-            System.out.println("Enter the elements in the array:");
-            for (int i = 0; i < size; i++) {
-                arr[i] = scanner.nextInt();
-            }
-            System.out.print("Enter the value of which you want to search: ");
-            int n = scanner.nextInt();
-            for(int i=0;i<arr.length;i++){
-                if(arr[i] == n){
-                    System.out.print(arr[i]);
-                }
+        int[] array = new int[length];
+
+        System.out.println("Enter " + length + " array elements:");
+        for (int i = 0; i < length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the value to search for: ");
+        int target = scanner.nextInt();
+
+        boolean found = false;
+        for (int i = 0; i < length; i++) {
+            if (array[i] == target) {
+                found = true;
+                System.out.println("Element " + target + " found at index " + i);
+                break;
             }
         }
+
+        if (!found) {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+
+        scanner.close();
+    }
 }
